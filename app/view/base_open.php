10c853747ecc8f2054464eab9c1736ce18568689
Juan Pedro Postigo <jppostigo97@gmail.com>
2017-12-11 18:59:48 +0100

    Demostración de link_css() y link_script().

--- app/view/base_open.php
+++ app/view/base_open.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 	<meta http-equiv="X-UA-Compatible" content="ie=edge" />
 	<title><?= title() ?></title>
-	<link rel="stylesheet" href="<?= APP_PATH ?>app/assets/css/style.css" />
-	<script src="<?= APP_PATH ?>app/assets/js/script.js"></script>
+	<?php link_css("style"); ?>
+	<?php link_script("script"); ?>
 </head>
 <body>
